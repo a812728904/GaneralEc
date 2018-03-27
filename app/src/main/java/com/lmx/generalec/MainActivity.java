2e41b9e4584e80c630e74db58cf93fdf7be8f6c8
812728904@qq.com <a844100970>
2018-03-27 11:16:41 +0800

    测试

--- app/src/main/java/com/lmx/generalec/MainActivity.java
+++ app/src/main/java/com/lmx/generalec/MainActivity.java
@@ -2,6 +2,7 @@ package com.lmx.generalec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.v("lmx","1");
     }
 }
