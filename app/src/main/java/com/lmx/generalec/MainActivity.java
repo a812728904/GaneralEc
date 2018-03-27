9a3a7148cb8ab01f97b86db7c5108ce3a59f8538
812728904@qq.com <a844100970>
2018-03-27 11:14:05 +0800

    测试

--- app/src/main/java/com/lmx/generalec/MainActivity.java
+++ app/src/main/java/com/lmx/generalec/MainActivity.java
@@ -2,8 +2,10 @@ package com.lmx.generalec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
